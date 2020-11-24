c0a5e915f1f1a9595fc483d44af136ed2d08ad04
sammy1193 <44882755+sammy1193@users.noreply.github.com>
2020-11-24 15:02:13 -0800

    Backend Code
    connection to database
    registering new users
    adding new students
    adding new teachers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //AccountGUI AGui = new AccountGUI();
-         //AGui.Display();
-        TeacherGUI TGui = new TeacherGUI("AHAHAH");
+        AccountGUI AGui = new AccountGUI();
+         AGui.Display();
+        //TeacherGUI TGui = new TeacherGUI("AHAHAH");
         //AdminGUI AdGui = new AdminGUI();
-        StudentInterface SI= new StudentInterface("Test");
-        AdminGUI aGUI = new AdminGUI("ADDD");
+        //StudentInterface SI= new StudentInterface("Test");
+        //AdminGUI aGUI = new AdminGUI("ADDD");
     }
 }
